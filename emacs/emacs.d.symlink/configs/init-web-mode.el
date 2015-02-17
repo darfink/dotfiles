2265f55d75ca150c0f59768cc8cab3a335db856c
Elliott Linder <elliott.darfink@gmail.com>
2015-02-17 17:19:46 +0100

    emacs improvements

---
+++ emacs/emacs.d.symlink/configs/init-web-mode.el
@@ -0,0 +1,40 @@
+(require 'web-mode)
+
+;; Prefer 2 spaces for indentation
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
+;; Connect file extensions to web-mode
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+
+;; Make web-mode play nice with smartparens
+(setq web-mode-enable-auto-pairing nil)
+
+(sp-with-modes '(web-mode)
+  (sp-local-pair "%" "%"
+                 :unless '(sp-in-string-p)
+                 :post-handlers '(((lambda (&rest _ignored)
+                                     (just-one-space)
+                                     (save-excursion (insert " ")))
+                                   "SPC" "=" "#")))
+  (sp-local-pair "<% "  " %>" :insert "C-c %")
+  (sp-local-pair "<%= " " %>" :insert "C-c =")
+  (sp-local-pair "<%# " " %>" :insert "C-c #")
+  (sp-local-tag "%" "<% "  " %>")
+  (sp-local-tag "=" "<%= " " %>")
+  (sp-local-tag "#" "<%# " " %>"))
+
+;; Sort of like matchit
+(plist-put evilmi-plugins web-mode
+           '((evilmi-html-get-tag web-mode-navigate)))
+
+(provide 'init-web-mode)
