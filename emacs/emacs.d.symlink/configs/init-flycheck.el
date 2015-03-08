6996db1f0f19a6be02d9279b11805f121c9d37cd
Elliott Linder <elliott.darfink@gmail.com>
2015-03-08 02:47:51 +0100

    init-flycheck.el: disable elisp document checks

--- emacs/emacs.d.symlink/configs/init-flycheck.el
+++ emacs/emacs.d.symlink/configs/init-flycheck.el
@@ -6,4 +6,7 @@
 (setq eldoc-idle-delay 0.1
       flycheck-display-errors-delay 0.2)
 
+(after 'flycheck
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
 (provide 'init-flycheck)
