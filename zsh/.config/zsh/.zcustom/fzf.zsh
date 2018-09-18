93036260947e5405dcd1e0e7f1b169ce2d8ede96
Elliott Linder <elliott.darfink@gmail.com>
2018-09-18 23:04:22 +0200

    feat(zsh): add C-M-t shortcut for fzf

--- zsh/.config/zsh/.zcustom/fzf.zsh
+++ zsh/.config/zsh/.zcustom/fzf.zsh
@@ -4,10 +4,6 @@ if [[ $- == *i* ]]; then
 
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail 2> /dev/null
   eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item} "
@@ -27,6 +23,10 @@ __fzfcmd() {
 }
 
 fzf-file-widget() {
+  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | cut -b3-"}"
   LBUFFER="${LBUFFER}$(__fsel)"
   local ret=$?
   zle redisplay
@@ -35,6 +35,17 @@ fzf-file-widget() {
 }
 zle     -N   fzf-file-widget
 bindkey '^T' fzf-file-widget
+
+fzf-file-all-widget() {
+  local cmd="$FZF_CTRL_T_COMMAND -H"
+  LBUFFER="${LBUFFER}$(__fsel)"
+  local ret=$?
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+  return $ret
+}
+zle     -N     fzf-file-all-widget
+bindkey '\e^T' fzf-file-all-widget
 
 # Ensure precmds are run after cd
 fzf-redraw-prompt() {
